fd4c0e8e4e3dfe6a171460e18cc684af45c4542a
MindNa <nalinee.phu@ku.th>
2019-09-15 07:11:30 +0700

    add abstract_factory

--- src/abstract_factory/DuckSimulator.java
+++ src/abstract_factory/DuckSimulator.java
@@ -3,47 +3,33 @@ package abstract_factory;
 public class DuckSimulator {
     public static void main(String[] args) {
         DuckSimulator simulator = new DuckSimulator();
-        //AbstractDuckFactory duckFactory = new CountingDuckFactory();
-        simulator.simulate();
+        AbstractDuckFactory duckFactory = new DuckFactory();
+        AbstractDuckFactory countingDuckFactory = new CountingDuckFactory();
+        AbstractDuckFactory echoAndCountDuckFactory = new EchoAndCountDuckFactory();
         //simulator.simulate(duckFactory);
+        //simulator.simulate(countingDuckFactory);
+        simulator.simulate(echoAndCountDuckFactory);
     }
-    void simulate() {
-        Quackable mallardDuck = new QuackCounter(new QuackEcho(new MallardDuck()));
-        Quackable redheadDuck = new QuackCounter(new QuackEcho(new RedheadDuck()));
-        Quackable duckCall = new QuackCounter(new QuackEcho(new DuckCall()));
-        Quackable rubberDuck = new QuackCounter(new QuackEcho(new RubberDuck()));
+    void simulate(AbstractDuckFactory factory) {
+        Quackable mallardDuck = factory.createMallardDuck();
+        Quackable redheadDuck = factory.createRedheadDuck();
+        Quackable duckCall = factory.createDuckCall();
+        Quackable rubberDuck = factory.createRubberDuck();
         Quackable goose = new GooseAdapter(new Goose());
         Quackable pigeon = new PigeonAdapter(new Pigeon());
-        System.out.println("\nDuck Simulator: With Decorator");
+
+        System.out.println("\nDuck Simulator: With Abstract");
+
         simulate(mallardDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(goose);
         simulate(pigeon);
+
         System.out.println("The ducks quacked " +
                 QuackCounter.getQuacks() + " times");
     }
-//    void simulate(AbstractDuckFactory duckFactory) {
-//        Quackable mallardDuck = duckFactory.createMallardDuck();
-//        Quackable redheadDuck = duckFactory.createRedheadDuck();
-//        Quackable duckCall = duckFactory.createDuckCall();
-//        Quackable rubberDuck = duckFactory.createRubberDuck();
-//        Quackable goose = new GooseAdapter(new Goose());
-//        Quackable pigeon = new PigeonAdapter(new Pigeon());
-//
-//        System.out.println("\nDuck Simulator: With Decorator");
-//
-//        simulate(mallardDuck);
-//        simulate(redheadDuck);
-//        simulate(duckCall);
-//        simulate(rubberDuck);
-//        simulate(goose);
-//        simulate(pigeon);
-//
-//        System.out.println("The ducks quacked " +
-//                QuackCounter.getQuacks() + " times");
-//    }
     void simulate(Quackable duck) {
         duck.quack();
     }
