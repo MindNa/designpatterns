af9b0fadeb2514f372a6a7dccc9f1d5ead514365
MindNa <nalinee.phu@ku.th>
2019-09-15 08:23:10 +0700

    add feature leader of duck in composite pattern

--- src/composite/Flock.java
+++ src/composite/Flock.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 
 public class Flock implements Quackable {
     ArrayList quackers = new ArrayList();
+    static int count = 0;
 
     public void add(Quackable quacker){
         quackers.add(quacker);
@@ -13,8 +14,13 @@ public class Flock implements Quackable {
     public void quack(){
         Iterator iterator = quackers.iterator();
         while (iterator.hasNext()){
+            count++;
             Quackable quacker = (Quackable) iterator.next();
             quacker.quack();
+            if (count == 1){
+                quacker.quack();
+                quacker.quack();
+            }
         }
     }
 }
